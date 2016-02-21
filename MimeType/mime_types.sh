1491b21c9b652b75f6c2d3343fc8bdf15a44f5e0
Trey Todd <TR3Y@Treys-Air.lan>
2016-02-20 21:11:41 -0800

    mod:mime

--- MimeType/mime_types.sh
+++ MimeType/mime_types.sh
@@ -2,22 +2,18 @@
 
 declare -A types
 
-toLower() {
-  echo $1 | tr '[:upper:]' '[:lower:]'
-}
-
 read N
 read Q
 
 for (( i=0; i<N; i++ )); do
   read EXT MT
-  ext=`toLower $EXT`
+  ext=$(echo $EXT | tr '[:upper:]' '[:lower:]')
   types[$ext]=$MT
 done
 
 for (( i=0; i<Q; i++ )); do
   read FNAME
-  name=`toLower $FNAME`
+  name=$(echo $FNAME | tr '[:upper:]' '[:lower:]')
   [[ $name == *"."* ]] && type=${name##*.} || type=
   echo ${types[$type]:-UNKNOWN}
 done
